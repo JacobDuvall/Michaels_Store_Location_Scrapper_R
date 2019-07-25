931cd5b997f7585fd0e468b04b9d06e1b507881e
Jacob Duvall <jdaleduvall@gmail.com>
2019-07-25 17:53:42 -0500

    Add files via upload

--- ac_moore.R
+++ ac_moore.R
@@ -1,17 +1,35 @@
 library(rvest)
 library(RODBC)
+library(xml2)
 
 devsqlswift_swiftstage = odbcDriverConnect('driver={SQL Server};
                                            server=DevSQLswift;
                                            database=SwiftStage;
                                            trusted_connection=True') 
 
-ac_moore = 'https://www.athome.com/store-find/'
+ac_moore = 'https://acmoore.com/stores'
 
 url = read_html(ac_moore)
 
 long_states = url %>%
-  html_nodes(".store-address span") %>%
+  html_nodes(".store-locator") %>%
   html_text() 
 
 print(long_states)
+
+system("C:/Users/jdduval1/Downloads/phantomjs-2.1.1-windows/phantomjs-2.1.1-windows/bin/phantomjs.exe C:\\Users\\jdduval1\\documents\\ac_moore.js")
+
+withJS = read_html("ac_moore.html") %>%
+  html_nodes("div#main-content") %>%
+  html_text()
+
+locations = strsplit(withJS, "\n\t\t\t\t\t\t")
+
+count = 1
+
+for (i in locations) {
+  i = gsub(",", "", i)
+  if (count ==   ) {
+    
+  }
+}
