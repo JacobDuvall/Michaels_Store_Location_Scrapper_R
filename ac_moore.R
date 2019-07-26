d07b7f41d52bcffbb71facf09a82404bd07a62ea
Jacob Duvall <jdaleduvall@gmail.com>
2019-07-25 20:20:40 -0500

    Add files via upload

--- ac_moore.R
+++ ac_moore.R
@@ -1,11 +1,5 @@
 library(rvest)
-library(RODBC)
 library(xml2)
-
-devsqlswift_swiftstage = odbcDriverConnect('driver={SQL Server};
-                                           server=DevSQLswift;
-                                           database=SwiftStage;
-                                           trusted_connection=True') 
 
 ac_moore = 'https://acmoore.com/stores'
 
@@ -17,19 +11,55 @@ long_states = url %>%
 
 print(long_states)
 
-system("C:/Users/jdduval1/Downloads/phantomjs-2.1.1-windows/phantomjs-2.1.1-windows/bin/phantomjs.exe C:\\Users\\jdduval1\\documents\\ac_moore.js")
-
+#system("C:/Users/jdduval1/Downloads/phantomjs-2.1.1-windows/phantomjs-2.1.1-windows/bin/phantomjs.exe C:\\Users\\jdduval1\\documents\\ac_moore.js")
+system('C:\\Users\\jdale\\Downloads\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe C:\\Users\\jdale\\OneDrive\\Documents\\R\\ac_moore.js')
 withJS = read_html("ac_moore.html") %>%
   html_nodes("div#main-content") %>%
   html_text()
 
-locations = strsplit(withJS, "\n\t\t\t\t\t\t")
+print(withJS)
 
-count = 1
 
-for (i in locations) {
-  i = gsub(",", "", i)
-  if (count ==   ) {
-    
+split = strsplit(withJS, "\"")[[1]]
+
+for(i in 1:length(split)) {
+  if(grepl("addMarker", split[i])) {
+    store = "A.C. Moore"
+    address = split[i+3]
+    phone = split[i+5]
+    city = split[i+7]
+    state = split[i+9]
+    zip = split[i+11]
+    if(grepl("font-size", split[i]) | grepl("google.maps", split[i])) {
+      next()
+    }
+    values = paste("(",
+                   "'",
+                   store,
+                   "'",
+                   ",",
+                   "'",
+                   address,
+                   "'",
+                   ",",
+                   "'",
+                   city,
+                   "'",
+                   ",",
+                   "'",
+                   state,
+                   "'",
+                   ",",
+                   "'",
+                   zip,
+                   "'",
+                   ",",
+                   "'",
+                   phone,
+                   "'",
+                   ")", sep = ""
+                   )
+    cmd = paste("INSERT INTO SwiftStage.Manual_Source.Competitor_Locations", values)
+    print(values)
   }
 }
